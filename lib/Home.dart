f793a5d805e2b1602b8857af195a90d2fbe19db5
赖海波 <1019832705@qq.com>
2022-03-04 15:08:54 +0800

    2022-03-04
    UI更新，核心功能尚未整合

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,9 +1,11 @@
 
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+const double myFontSize = 13;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -13,30 +15,322 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  final TextEditingController _modelFileNameController = TextEditingController();
+
+  final TextEditingController _modelRootNameController = TextEditingController();
+
+  final TextEditingController _modelJsonController = TextEditingController();
+
+  final TextEditingController _modelPreviewJsonController = TextEditingController();
+
+  bool _allowOverride = true;
+
+  bool _allString = true;
+
+  bool _needFinal = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("widget.title"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:',),
-            Text('', style: Theme.of(context).textTheme.headline4,),
+      body: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+        child: Row(
+          children: [
+            Expanded(
+              child: _leftWidget(),
+              flex: 2,
+            ),
+            const VerticalDivider(width: 1,color: Colors.black,),
+            Expanded(
+              child: _rightWidget(),
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async{
-          // String? selectedDirectory = await FilePicker.platform.getDirectoryPath();// file_picker: ^4.4.0
-          _readJSON();
+    );
+  }
 
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  //左侧的文本输入部分
+  Widget _leftWidget(){
+    return Container(
+      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+                borderRadius: BorderRadius.all(Radius.circular(3.0)),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.green),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              disabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.red),
+                borderRadius: BorderRadius.all(Radius.circular(0.0)),
+              ),
+              labelText: "请输入文件名",
+              labelStyle: TextStyle(color: Colors.black87,fontSize: myFontSize),
+              floatingLabelStyle: TextStyle(color: Colors.green),
+            ),
+            controller: _modelFileNameController,
+            autocorrect:false,
+            style: const TextStyle(color: Colors.black,fontSize: myFontSize),
+          ),
+          const SizedBox(height: 10,),
+          TextField(
+            decoration: const InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+                borderRadius: BorderRadius.all(Radius.circular(3.0)),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.green),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              disabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.red),
+                borderRadius: BorderRadius.all(Radius.circular(0.0)),
+              ),
+              labelText: "请输入Model根名",
+              labelStyle: TextStyle(color: Colors.black87,fontSize: myFontSize),
+              floatingLabelStyle: TextStyle(color: Colors.green),
+            ),
+            controller: _modelRootNameController,
+            autocorrect:false,
+            style: const TextStyle(color: Colors.black,fontSize: myFontSize),
+          ),
+          const Text("例如：输入'Automatic',输出为'AutomaticModel/AutomaticData',具体可以在设置中更改",style: TextStyle(fontSize: 10,color: Colors.lightBlue),),
+          const SizedBox(height: 10,),
+          Expanded(
+            child: TextField(
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+                disabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                  borderRadius: BorderRadius.all(Radius.circular(0.0)),
+                ),
+                labelText: "请输入接口报文JSON",
+                labelStyle: TextStyle(color: Colors.black87,fontSize: myFontSize),
+                floatingLabelStyle: TextStyle(color: Colors.green),
+                alignLabelWithHint: true,
+              ),
+              maxLines: null,
+              minLines: null,
+              expands: true,
+              textAlignVertical:TextAlignVertical.top,
+              controller: _modelJsonController,
+              autocorrect:false,
+              style: const TextStyle(color: Colors.black,fontSize: myFontSize),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  //右侧的选项部分
+  Widget _rightWidget(){
+    return Container(
+      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+      alignment: Alignment.center,
+      color: Colors.white,
+      child: Column(
+        children: [
+          const Text("基本选项",style: TextStyle(fontSize: myFontSize + 2,fontWeight: FontWeight.w600),),
+          const SizedBox(height: 5,),
+          const Divider(height: 1,color: Colors.black,),
+          const SizedBox(height: 5,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  const Text("允许文件覆盖",style: TextStyle(fontSize: myFontSize,fontWeight: FontWeight.w600)),
+                  IconButton(
+                    onPressed: (){
+                      ScaffoldMessenger.of(context).clearSnackBars();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          action: SnackBarAction(label: "了解!",onPressed: (){
+                            ScaffoldMessenger.of(context).clearSnackBars();
+                          },),
+                          content:const Text('启用此选项后，将允许直接覆盖同名文件；关闭后，将提示用户进行修改',style: TextStyle(fontSize: 12),),
+                          backgroundColor: Colors.orange,
+                          padding:const EdgeInsets.fromLTRB(10, 15, 10, 15),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.question_answer,color: Colors.orangeAccent,size: 15,),),
+                ],
+              ),
+              CupertinoSwitch(
+                value: _allowOverride,
+                onChanged:(e){
+                  setState(() {
+                    _allowOverride = e;
+                  });
+                },
+              ),
+            ],
+          ),
+          const SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  const Text("启用类型全String",style: TextStyle(fontSize: myFontSize,fontWeight: FontWeight.w600)),
+                  IconButton(
+                    onPressed: (){
+                      ScaffoldMessenger.of(context).clearSnackBars();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                         SnackBar(
+                          action: SnackBarAction(label: "了解!",onPressed: (){
+                            ScaffoldMessenger.of(context).clearSnackBars();
+                          },),
+                          content:const Text('启用此选项后，将除了List，Map类型外的其他参数类型转换为String类型；关闭后，将使用参数的原本类型',style: TextStyle(fontSize: 12),),
+                          backgroundColor: Colors.orange,
+                          padding:const EdgeInsets.fromLTRB(10, 15, 10, 15),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.question_answer,color: Colors.orangeAccent,size: 15,),),
+                ],
+              ),
+              CupertinoSwitch(
+                value: _allString,
+                onChanged:(e){
+                  setState(() {
+                    _allString = e;
+                  });
+                },
+              ),
+            ],
+          ),
+          const SizedBox(height: 10,),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  const Text("final修饰词",style: TextStyle(fontSize: myFontSize,fontWeight: FontWeight.w600)),
+                  IconButton(
+                    onPressed: (){
+                      ScaffoldMessenger.of(context).clearSnackBars();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          action: SnackBarAction(label: "了解!",onPressed: (){
+                            ScaffoldMessenger.of(context).clearSnackBars();
+                          },),
+                          content:const Text('启用此选项后，将在参数前添加final，例如final String? id；关闭后为String? id',style: TextStyle(fontSize: 12),),
+                          backgroundColor: Colors.orange,
+                          padding:const EdgeInsets.fromLTRB(10, 15, 10, 15),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.question_answer,color: Colors.orangeAccent,size: 15,),),
+                ],
+              ),
+              CupertinoSwitch(
+                value: _needFinal,
+                onChanged:(e){
+                  setState(() {
+                    _needFinal = e;
+                  });
+                },
+              ),
+            ],
+          ),
+          const SizedBox(height: 5,),
+          const Divider(height: 1,color: Colors.black,),
+          const SizedBox(height: 10,),
+
+          ElevatedButton(
+            child: Container(
+              child: const Text("生成预览",maxLines: 1,overflow: TextOverflow.ellipsis,softWrap: true,),
+              padding: const EdgeInsets.fromLTRB(10, 8, 10, 8),
+              alignment: Alignment.center,
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.amber,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(6),
+              ),
+            ),
+            onPressed: () async{
+              _readJSON();
+            },
+          ),
+          const SizedBox(height: 10,),
+          ElevatedButton(
+            child: Container(
+              child: const Text("生成Model文件",maxLines: 1,overflow: TextOverflow.ellipsis,softWrap: true,),
+              padding: const EdgeInsets.fromLTRB(10, 8, 10, 8),
+              alignment: Alignment.center,
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.green,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(6),
+              ),
+            ),
+            onPressed: (){
+
+              // String? selectedDirectory = await FilePicker.platform.getDirectoryPath();// file_picker: ^4.4.0
+              _readJSON();
+            },
+          ),
+          const SizedBox(height: 10,),
+          Expanded(
+            child: TextField(
+              readOnly: true,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+                disabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber),
+                  borderRadius: BorderRadius.all(Radius.circular(0.0)),
+                ),
+                labelText: "点击'生成预览'后这里会展示生成Model",
+                labelStyle: TextStyle(color: Colors.black87,fontSize: 8),
+                floatingLabelStyle: TextStyle(color: Colors.amber),
+                alignLabelWithHint: false,
+              ),
+              maxLines: null,
+              minLines: null,
+              expands: true,
+              textAlignVertical:TextAlignVertical.top,
+              controller: _modelPreviewJsonController,
+              autocorrect:false,
+              style: const TextStyle(color: Colors.black,fontSize: 8),
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 
@@ -78,29 +372,48 @@ class _MyHomePageState extends State<MyHomePage> {
     File file = File("/Volumes/mac/Users/biex/Downloads/autogeneratemodel/lib/json.json");
     String str = await file.readAsString();
     Map _jsonMap = jsonDecode(str);
-    _makeModel(_jsonMap);
-
+    // _makeModel(_jsonMap);
+    _previewModel(_jsonMap);
   }
 
   //制作
-  _makeModel(Map map) async{
+  _makeModel(Map map) async {
     List _resultList = [];
     int _recursionKey = 1;
     const String _modelRootName = 'neverRootResponse';
 
     bool _allString = true;
 
-    _resultList = await _disassemblyModel(map,_recursionKey,rootModelName: _modelRootName,allString: _allString);
+    _resultList = await _disassemblyModel(map, _recursionKey, rootModelName: _modelRootName, allString: _allString);
 
     //第四步写入文件
     String _result = '';
-    for(var it in _resultList){
+    for (var it in _resultList) {
       _result += it + '\n';
     }
-    _createFile(_result,fileName: "automodel",fileOverwriteAlreadyExists: true);
-
-
+    _createFile(_result, fileName: "automodel", fileOverwriteAlreadyExists: true);
   }
+
+  _previewModel(Map map) async{
+    List _resultList = [];
+    int _recursionKey = 1;
+    const String _modelRootName = 'neverRootResponse';
+
+    bool _allString = true;
+
+    _resultList = await _disassemblyModel(map, _recursionKey, rootModelName: _modelRootName, allString: _allString);
+
+    //第四步写入文件
+    String _result = '';
+    for (var it in _resultList) {
+      _result += it + '\n';
+    }
+
+    setState(() {
+      _modelPreviewJsonController.text = _result;
+    });
+  }
+
 
 
   //原json，递归层数，model根姓名，是否除List外全部使用String
